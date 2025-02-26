1beb9a2840d3f51dac29a20f4744f3e5f8b56d38
Tahan Shakil <tahsanknmas14@gmail.com>
2025-02-26 21:01:11 +0600

    complete login page

---
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,61 @@
+import card2 from "../../assets/card2.png";
+import { LuTrendingUpDown } from "react-icons/lu";
+import PropTypes from "prop-types";
+
+const StatsInfoCard = ({ icon, label, value, color }) => {
+  return (
+    <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10">
+      <div
+        className={`w-12 h-12 flex justify-center items-center text-[26px] text-white ${color} rounded-full drop-shadow-lg`}
+      >
+        {icon}
+      </div>
+      <div className="mt-2">
+        <h5 className="text-gray-500 text-xs mb-1">{label}</h5>
+        <p className="text-[20px]">${value}</p>
+      </div>
+    </div>
+  );
+};
+
+StatsInfoCard.propTypes = {
+  icon: PropTypes.node.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+        {children}
+      </div>
+      <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+        <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"></div>
+        <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10"></div>
+        <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5"></div>
+        <div className="grid grid-cols-1 z-20">
+          <StatsInfoCard
+            icon={<LuTrendingUpDown />}
+            label="Track Your Income $ Expenses "
+            value="430000"
+            color="bg-primary"
+          />
+        </div>
+        <img
+          src={card2}
+          alt=""
+          className="w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15"
+        />
+      </div>
+    </div>
+  );
+};
+
+AuthLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthLayout;
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../../components/layouts/inputs/Input";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  //handleLogin
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("All fields are required");
+      return;
+    }
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+    if (!email.includes("@")) {
+      setError("Email must be valid");
+      return;
+    }
+    navigate("/dashboard");
+  };
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">welcome back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          please enter your details to log in
+        </p>
+        {/*  */}
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="shakil@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="min 6 characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs mt-2">{error}</p>}
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signUp">
+              Signup
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Login;
