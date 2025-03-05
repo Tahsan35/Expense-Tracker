37476ceff1a5f0377c620696ae4ac0bfd45e61d5
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-05 23:50:22 +0600

    edit protect route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -63,7 +63,16 @@ const loginUser = async (req, res) => {
 };
 //get user info
 const getUserInfo = async (req, res) => {
-  const user = req.user;
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
 };
 
 export { registerUser, loginUser, getUserInfo };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,31 +2,46 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
-  let token;
+  let token = req.headers.authorization?.split(" ")[1];
 
-  // Check if token exists in headers
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ message: "Not authorized, no token" });
 
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Get user from token
-      req.user = decoded;
-      
-      next();
-    } catch (error) {
-      console.error('Authentication error:', error);
-      res.status(401).json({ message: 'Not authorized, token failed' });
-    }
-  }
-
-  if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userId).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Not authorized, token failed" });
   }
 };
+
+// export const protect = async (req, res, next) => {
+//   let token;
+
+//   // Check if token exists in headers
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith("Bearer")
+//   ) {
+//     try {
+//       // Get token from header
+//       token = req.headers.authorization.split(" ")[1];
+
+//       // Verify token
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//       // Get user from token
+//       req.user = decoded;
+
+//       next();
+//     } catch (error) {
+//       console.error('Authentication error:', error);
+//       res.status(401).json({ message: 'Not authorized, token failed' });
+//     }
+//   }
+
+//   if (!token) {
+//     res.status(401).json({ message: 'Not authorized, no token' });
+//   }
+// };
