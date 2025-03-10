de8d4642a68fcda4f1f5446533845ce07828870e
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-11 00:17:32 +0600

    create userHookAuth

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -1,9 +1,57 @@
 import PropTypes from "prop-types";
+import { SIDE_MENU_DATA } from "../../utils/data";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import { useNavigate } from "react-router-dom";
 
 const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if (route === "/logout") {
+      handleLogout();
+      return;
+    }
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
+      <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
+        {user?.profileImageUrl ? (
+          <img
+            src={user?.profileImageUrl || ""}
+            alt="Profile Image"
+            className="w-20 h-20 bg-slate-400 rounded-full"
+          />
+        ) : (
+          <></>
+        )}
+        <h5 className="text-gray-950 font-medium leading-6">{user?.name || ""}</h5>
+      </div>
+
+      {SIDE_MENU_DATA.map((item, index) => (
+        <button
+          key={`menu_${index}`}
+          className={`flex items-center gap-4 text-[15px] w-full ${
+            activeMenu == item.label
+              ? "text-white bg-primary"
+              : "text-gray-600 hover:bg-gray-100"
+          } py-3 px-6 rounded-lg mb-3`}
+          onClick={() => handleClick(item.path)}
+        >
+          <item.icon className="text-xl" />
+          <span className="flex-1 text-left">{item.label}</span>
+        </button>
+      ))}
     </div>
   );
 };
