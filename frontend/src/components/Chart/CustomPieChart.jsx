8be1549ab75baddb2804a882251d5856d0571225
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-12 16:13:50 +0600

    financial overview pie chart

--- frontend/src/components/Chart/CustomPieChart.jsx
+++ frontend/src/components/Chart/CustomPieChart.jsx
@@ -1,3 +1,14 @@
+import {
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Cell,
+  Tooltip,
+  Legend,
+} from "recharts";
+import customTooltip from "./customTooltip";
+import customLegend from "./customLegend";
+
 const CustomPieChart = ({
   data,
   label,
@@ -5,7 +16,53 @@ const CustomPieChart = ({
   colors,
   showTextAnchor,
 }) => {
-  return <div>CustomPieChart</div>;
+  return (
+    <ResponsiveContainer width="100%" height={380}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="amount"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={130}
+          innerRadius={100}
+          labelLine={false}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
+        <Tooltip content={customTooltip} />
+        <Legend content={customLegend} />
+        {showTextAnchor && (
+          <>
+            <text
+              x="50%"
+              y="50%"
+              dy={-25}
+              textAnchor="middle"
+              fill="#666"
+              fontSize="14px"
+            >
+              {label}
+            </text>
+            <text
+              x="50%"
+              y="50%"
+              dy={8}
+              textAnchor="middle"
+              fill="#333"
+              fontSize="24px"
+              fontWeight="semi-bold"
+            >
+              {totalAmount}
+            </text>
+          </>
+        )}
+      </PieChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default CustomPieChart;
