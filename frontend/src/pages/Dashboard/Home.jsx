aebf37469edb6e05f6d1b1abcb4123a5e2a74fef
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-16 01:29:27 +0600

    overview

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
           <InfoCard
             icon={<LuHandCoins />}
             label="Total Expense"
-            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            value={addThousandsSeparator(dashboardData?.totalExpenses || 0)}
             color="bg-red-500"
             onClick={() => navigate("/products")}
           />
